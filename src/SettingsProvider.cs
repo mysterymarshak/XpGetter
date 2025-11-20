b45ae02e55b4bbbb97bf7dcbb6efa4fdd940bd30
mysterymarshak <test1132100948@gmail.com>
2025-11-20 21:35:20 +0500

    Fix the warnings

--- src/SettingsProvider.cs
+++ src/SettingsProvider.cs
@@ -70,11 +70,11 @@ public class Account
     public ulong Id { get; set; }
 
     [JsonProperty("access_token")]
-    public string AccessToken { get; set; }
+    public string AccessToken { get; set; } = string.Empty;
 
     [JsonProperty("refresh_token")]
-    public string RefreshToken { get; set; }
+    public string RefreshToken { get; set; } = string.Empty;
 
     [JsonProperty("username")]
-    public string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
 }
